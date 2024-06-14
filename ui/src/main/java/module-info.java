bfd148087fedc66a013be3cd6e042d537b6908be
Leon Junio Martins Ferreira <juniohuy2gamer@gmail.com>
2024-06-14 16:16:46 -0300

    Fix all confs and fix folders

---
+++ ui/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module boisbarganhados {
+    requires transitive javafx.graphics;
+    requires transitive lombok;
+    requires transitive javafx.swing;
+    requires transitive org.bytedeco.opencv;
+    requires javafx.controls;
+    requires transitive javafx.fxml;
+    requires java.desktop;
+    requires atlantafx.base;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.material2;
+    requires org.bytedeco.javacv;
+    requires com.formdev.flatlaf;
+
+    opens boisbarganhados to javafx.fxml;
+    exports boisbarganhados;
+}
