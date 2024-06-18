c1dbf57aa14313281ab29ec92d481314a0f692c2
Leon Junio Martins Ferreira <juniohuy2gamer@gmail.com>
2024-06-17 23:14:30 -0300

    loading

--- ui/src/main/java/boisbarganhados/App.java
+++ ui/src/main/java/boisbarganhados/App.java
@@ -12,7 +12,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 import atlantafx.base.theme.PrimerDark;
-import boisbarganhados.python_layer.Requirements;
 
 public class App extends Application {
 
@@ -22,7 +21,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
         FXMLLoader loader = loadFXML("loadingDependencies");
-        scene = new Scene(loader.load(), 800, 600);
+        scene = new Scene(loader.load(), 600, 400);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         scene.setFill(Color.TRANSPARENT);
         stage.initStyle(javafx.stage.StageStyle.TRANSPARENT);
@@ -31,57 +30,8 @@ public class App extends Application {
         stage.getIcons().add(new Image(getClass().getResourceAsStream("logo.png")));
         stage.setResizable(true);
         stage.show();
-        Task<Void> taskMain = new Task<>() {
-            @Override
-            protected Void call() {
-        if (!Requirements.loadRequirements()) {
-            System.err.println("Failed to load requirements");
-            System.exit(1);
-        }
-        return null;
-            }
-        };
-
-        taskMain.setOnSucceeded(e -> {
-            try {
-                startMainScreen(stage);
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        });
-
-        taskMain.setOnFailed(e -> {
-            System.err.println("Failed to load requirements");
-            System.exit(1);
-        });
-
-        Service<Void> service = new Service<>() {
-            @Override
-            protected Task<Void> createTask() {
-                return taskMain;
-            }
-        };
-
-        service.start();
-        
-    }
-
-    public void startMainScreen(Stage stage) throws IOException{
-        stage.close();
-        stage = new Stage();
-        FXMLLoader loader = loadFXML("primary");
-        scene = new Scene(loader.load(), 1200, 800);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-        scene.setFill(Color.TRANSPARENT);
-        PrimaryController controller = loader.getController();
-        controller.setStage(stage);
-        stage.initStyle(javafx.stage.StageStyle.TRANSPARENT);
-        stage.setScene(scene);
-        stage.setTitle("Papanicolau Image Viewer and Classifier");
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("logo.png")));
-        stage.setResizable(true);
-        stage.show();
-        controller.startKeyBindings();
+        DependenciesLoadingController controller = loader.getController();
+        controller.loadDependencies(stage, scene);
     }
 
     static void setRoot(String fxml) throws IOException {
